bde5e4dc79122f17239492a4c840a9a74d3d8701
dnjoe96 <donjoedbest@gmail.com>
2022-07-07 23:39:14 -0700

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,14 +10,18 @@ import sys
 
 
 class Log:
-    """ Log class"""
+    """ 
+    Log class
+    """
     count = 0
     sum = 0
     status_code = {}
     code_choice = [200, 301, 400, 401, 403, 404, 405, 500]
 
     def process() -> None:
-        """ Processes the log data"""
+        """ 
+        Processes the log data
+        """
         # print(status_code)
         print(f"File size: {Log.sum}")
         for code in Log.code_choice:

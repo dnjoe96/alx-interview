e2a827bc12f41ab366544af9529550e7da5ef6f7
dnjoe96 <donjoedbest@gmail.com>
2022-07-14 17:59:55 +0100

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,11 +2,10 @@
 """ Module for UTF8_data class """
 
 
-class Unicode:
-    """ Unicode class """
-    def validUTF8(data_set):
-        """ A function to validate if a data set is a valid utf-8 format """
-        for elem in data_set:
-            if elem > 255 or elem < 0:
-                return False
-            return True
+def validUTF8(data_set):
+    """ A function to validate if a data set is a valid utf-8 format """
+    for elem in data_set:
+        # print(elem)
+        if elem > 255 or elem < 0:
+            return False
+    return True

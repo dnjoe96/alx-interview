fa856b6d617d8acc45e91da24d7d51dff8e75e18
dnjoe96 <donjoedbest@gmail.com>
2022-09-09 00:02:22 +0100

    0-prime_game.py

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Solution Module"""
+
+
+def isWinner(x, nums):
+    """ function to find winner
+    Args:
+        x: int - rounds
+        nums: List - numbers list
+    Return:
+        winner
+    """
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
+        return None
+
+    ben = 0
+    maria = 0
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
